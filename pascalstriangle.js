5283e8a8a6741fa06a1e41a6259359c009579890
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2022-02-05 15:58:54 -0700

    pascals triangle

---
+++ pascalstriangle.js
@@ -0,0 +1,15 @@
+function generate(numRows) {
+    let triangle = []
+    
+    for (let i = 0; i < numRows; i++) {
+        triangle[i] = []
+        triangle[i][0] = 1
+        triangle[i][i] = 1
+        
+        for (let j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+        }
+    }
+    return triangle
+    
+};
